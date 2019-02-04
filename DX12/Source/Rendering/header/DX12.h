76384fbd259cc919c24374f387e132452f7d3374
Unknown <K.schaefer92@gmx.de>
2019-02-04 20:40:16 +0100

    [WIP] basic implementation of projects.

---
+++ DX12/Source/Rendering/header/DX12.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <d3d12.h>
+
+
+namespace Rendering
+{
+	class DX12
+	{
+	public: 
+
+		DX12();
+		~DX12();
+
+	private: 
+
+	};
+}
---
+++ DX12/Source/Rendering/src/DX12.cpp
@@ -0,0 +1,9 @@
+#include "..\header\DX12.h"
+
+Rendering::DX12::DX12()
+{
+}
+
+Rendering::DX12::~DX12()
+{
+}
